c3812184d0ef7b5323a6f5f951c862e713bd1425
Keloc13 <kvnyn10@gmail.com>
2018-02-04 16:56:54 -0800

    Added gif example

--- Flix_Part_1/NowPlayingViewController.swift
+++ Flix_Part_1/NowPlayingViewController.swift
@@ -11,32 +11,30 @@ import AlamofireImage
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var movies: [[String: Any]] = []
+    
+   
+    
+    var refreshControl: UIRefreshControl!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        activityIndicator.startAnimating()
+        refreshControl = UIRefreshControl()
+       
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
         tableView.delegate = self
 
-       // tableView.
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-       
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                let dataDictionary  = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                let movies = dataDictionary["results"] as! [[String: Any]]
-                self.movies = movies
-               // print(dataDictionary)
-                self.tableView.reloadData()
-            }
-        }
-        // Do any additional setup after loading the view.
-        task.resume()
+        fetchMovies()
+        activityIndicator.stopAnimating()
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        fetchMovies()
     }
         
 
@@ -63,7 +61,28 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        
         return cell
+    }
+    
+    func fetchMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary  = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+            }
+        }
+        // Do any additional setup after loading the view.
+        task.resume()
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
