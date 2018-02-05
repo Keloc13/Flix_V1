7271f9deb16522258f323a218fdf36919dfc2069
Keloc13 <kvnyn10@gmail.com>
2018-02-05 09:52:53 -0800

    Fixed formatting of TableView and Loading icon

--- Flix_Part_1/NowPlayingViewController.swift
+++ Flix_Part_1/NowPlayingViewController.swift
@@ -18,26 +18,18 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         activityIndicator.startAnimating()
+        self.activityIndicator.startAnimating()
         tableView.dataSource = self
         tableView.delegate = self
-        
-       
         refreshControl = UIRefreshControl()
-       
         refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
-        
         tableView.insertSubview(refreshControl, at: 0)
-        
-
         fetchMovies()
-        activityIndicator.stopAnimating()
     }
     
     @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
         fetchMovies()
     }
-        
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -45,10 +37,13 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.activityIndicator.startAnimating()
+       // self.activityIndicator.stopAnimating()
         return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        self.activityIndicator.startAnimating()
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
         let movie = movies[indexPath.row]
@@ -56,13 +51,13 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         let overview = movie["overview"] as! String
         let posterPathString = movie["poster_path"] as! String
         let baseURLString = "https://image.tmdb.org/t/p/w500"
-        
         let posterURL = URL(string: baseURLString + posterPathString)!
         
         cell.posterImageView.af_setImage(withURL: posterURL)
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        self.activityIndicator.stopAnimating()
         
         return cell
     }
@@ -89,7 +84,10 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     {
+       // self.activityIndicator.startAnimating()
+       
         return 130.0;//Choose your custom row height
+        
     }
 
 }
